2e499d5b21ee88d6d8b4cb0674efd657dcd78389
Prakash Balakrishnan <prakashindia1294@gmail.com>
2025-12-21 10:07:57 +0530

    Java Comments Learning

---
+++ CoreJava/HelloWorld.java
@@ -0,0 +1,31 @@
+/**
+ * This class to learn about comments in java.
+ * Java Doc comment block
+ * @author HelloWorld
+ */
+
+class HelloWorld {
+    /**
+     * This the main method to print "Hello Word"
+     * There are many tag in the java comments, some are listed bellow.
+     * @param args input
+     * {@value used for static field. Displays a value of constants}
+     * {@code Type you code here}
+     * @exception specify the type exception what to caught
+     * @throws same as exception tag
+     * @see reference link to paste
+     * @return nothing
+     * @version 1.0 adding version info of class file.
+     */
+    public static void main(String[] args) {
+        /*
+        * This is multi line comment.
+        * which can you used to explain the code block
+        * The below line print the Hello World.
+        * Do not delete the block.
+        */
+        System.out.println("Hellow world");
+        // This is single line comment
+        // TODO add more code in future.
+    }
+}
